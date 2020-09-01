515388d8e563ee3829c9c9ddee914da57facdc83
IG Kim <59094101+IanKim2511@users.noreply.github.com>
2020-09-01 16:53:33 -0400

    js 58. show navbar with scrolling. make navbar transparnet when it is on the top

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// js 58. show navbar with scrolling. make navbar transparnet when it is on the top
+
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll',()=>{
+    if (window.scrollY>navbarHeight) {
+        navbar.classList.add('navbar--dark');
+        
+    } else {
+        
+        navbar.classList.remove('navbar--dark');
+    }
+});
