a5890501fe777213abdc1d274eb628382857bafb
IG Kim <59094101+IanKim2511@users.noreply.github.com>
2020-09-02 19:12:50 -0400

    work__projects

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-'use strict';
+/*  I numberd the codes to connect to the other code.
+ so you can find 'js 72' at JS file if you see 'js 72' at CSS or HTML file. 
+*/
+
+"use strict";
 
 // js 58. show navbar with scrolling. make navbar transparnet when it is on the top
 
-
-const navbar = document.querySelector('#navbar');
+const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-document.addEventListener('scroll',()=>{
-    if (window.scrollY>navbarHeight) {
-        navbar.classList.add('navbar--dark');
-        
-    } else {
-        
-        navbar.classList.remove('navbar--dark');
-    }
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
 });
 
+// js 70. project button animation
 
-// js 72. select the next selection and remove selection from the previous item  
+// projectContainer.classList.add('anim-out');
 
-window.addEventListener('click',(e)=>{
-    
-const active = document.querySelector('.category__btn.selected');
-if (active!=null) {
-    
-    active.classList.remove('selected');
-}
-e.target.classList.add('selected');
+// setTimeout(() => {
+//     projectContainer.classList.remove('anim-out');
+// }, 300);
 
+// js 72. select the next selection and remove selection from the previous item
+
+window.addEventListener("click", (e) => {
+  const active = document.querySelector(".category__btn.selected");
+  if (active != null) {
+    active.classList.remove("selected");
+  }
+  e.target.classList.add("selected");
 });
