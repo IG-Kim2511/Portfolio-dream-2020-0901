54cd503839383c65d68eda98abbb19b79125c9b9
IG Kim <59094101+IanKim2511@users.noreply.github.com>
2020-09-04 23:51:55 -0400

    add: wip

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-/*  I numberd the codes to connect to the other code.
- so you can find 'js 72' at JS file if you see 'js 72' at CSS or HTML file. 
-*/
+// # I numberd the codes to connect to the other code.
+// # for example, if you see 'js 72' at CSS or HTML file, you can find 'js 72' at JS file
 
 "use strict";
 
