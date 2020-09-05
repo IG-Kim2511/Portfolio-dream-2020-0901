17e0c3aeebb836c45ffccaffe1fe550e3e2e0329
IG Kim <59094101+IanKim2511@users.noreply.github.com>
2020-09-04 23:58:12 -0400

    // js 94  make home slowly fade to transparent as the window scrolls down

--- main.js
+++ main.js
@@ -68,12 +68,12 @@ navbarToggleBtn.addEventListener("click", () => {
 
 // js 94  make home slowly fade to transparent as the window scrolls down
 
-const home = document.querySelector(".home__container");
-const homeHeight = home.getBoundingClientRect().height;
+// const home = document.querySelector(".home__container");
+// const homeHeight = home.getBoundingClientRect().height;
 
-document.addEventListener("scroll", () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight;
-});
+// document.addEventListener("scroll", () => {
+//   home.style.opacity = 1 - window.scrollY / homeHeight;
+// });
 
 // js 90. Show "arrow up" button when scrolling down
 
